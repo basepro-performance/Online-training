003b506fa0fcdec23ed3430c16b272df8c9f6284
basepro-performance <comercial@baseprosport.com.br>
2026-02-10 11:44:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-const CACHE_NAME = 'basepro-v6';
+const CACHE_NAME = 'basepro-v7'; // Mudei para v7 para forçar o navegador a ler de novo
 const assets = ['index.html', 'manifest.json'];
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Força a ativação imediata
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
 });
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
